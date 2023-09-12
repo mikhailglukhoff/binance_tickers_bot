ac755357f341f9671d6ac12856a1b3ab36cf830e
Mikhail Glukhov <michaelglukhoff@gmail.com>
2023-09-12 14:20:53 +0300

    send clear data.py

---
+++ data.py
@@ -0,0 +1,12 @@
+binance_api = {'api_key': '',
+               'secret_key': ''
+               }
+
+tickers = [
+    'BTCUSDT',
+    'BNBUSDT',
+    'USDTRUB',
+    'USDTTRY'
+]
+
+telegram_bot_token = ''
